3857598301b98fada446b15f26314cf387d9f478
Garth Lapitan <garthlapitan.up@gmail.com>
2020-11-24 21:40:10 +0800

    Refactor: remove perfTest.js

--- src/perfTest.js
+++
@@ -1,6 +0,0 @@
-var timeExec;
-function perfTest(name) {
-  let newTimeExec = performance.now();
-  if (name != "x") console.log(`${name}: ${newTimeExec - timeExec} ms`);
-  timeExec = newTimeExec;
-}
