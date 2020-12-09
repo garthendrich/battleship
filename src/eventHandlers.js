f8569bfbee3ae1e9d1fd0f53fee305985c207554
Garth Lapitan <garthlapitan.up@gmail.com>
2020-12-10 01:52:57 +0800

    refactor: split main.js into multiple files

---
+++ src/eventHandlers.js
@@ -0,0 +1,109 @@
+// mousedown on ship menu item
+const menuShipElems = document.querySelectorAll(".ship-menu__item");
+menuShipElems.forEach((elem) =>
+  elem.addEventListener("mousedown", (e) => {
+    if (!e.target.classList.contains(".ship-menu__item--placed")) user.selectedShip = elem.id;
+  })
+);
+
+var selectedShipElem, canMoveShip, shipPointOnMousedown, selectedShipPrevOrigin;
+document.body.addEventListener("mousedown", (e) => {
+  // if mousedown outside board
+  if (!e.target.closest(".board")) {
+    hideAllShipPopups();
+    return;
+  }
+
+  // if previous dragging did not mouseup on body, skip collecting new ship info
+  if (user.selectedShip) return;
+
+  // if mousedown on ship, collect ship info
+  selectedShipElem = e.target.classList.contains("ship") ? e.target : null;
+  if (selectedShipElem) {
+    canMoveShip = true;
+    user.selectedShip = selectedShipElem.id;
+    shipPointOnMousedown = getCurrentShipPointUnderCursor(e);
+    selectedShipPrevOrigin = user.shipOrigin[user.selectedShip];
+  }
+});
+
+document.body.addEventListener("mousemove", (e) => {
+  // if mousedown on ship
+  if (canMoveShip) {
+    // if ship will be moved -- if cursor outside selected ship || cursor outside cell where it started mousedown
+    if (!(e.target.id == user.selectedShip) || shipPointOnMousedown != getCurrentShipPointUnderCursor(e)) {
+      canMoveShip = false; // reset; pass only once
+      removeSelectedShip();
+      hideAllShipPopups();
+    }
+  }
+});
+
+document.body.addEventListener("mouseup", (e) => {
+  canMoveShip = false; // reset
+  hideAllShipPopups();
+
+  if (user.selectedShip) {
+    // if mouseup on same ship
+    if (e.target.classList.contains("ship") && e.target.id == user.selectedShip) e.target.firstChild.style.display = null;
+    // else if mouseup on cell
+    else if (e.target.nodeName == "TD") {
+      const rowUnderCursor = e.target.closest("tr").rowIndex;
+      const columnUnderCursor = e.target.cellIndex;
+      user.selectedShipOrigin = [rowUnderCursor, columnUnderCursor];
+      user.adjustShipCellOriginToInsideBoard(shipPointOnMousedown);
+
+      if (!user.doesSelectedShipOverlapOthers()) user.addShip();
+      else if (selectedShipPrevOrigin) user.addShip(selectedShipPrevOrigin);
+    } // else if mouseup not on cell
+    else if (selectedShipPrevOrigin) user.addShip(selectedShipPrevOrigin);
+  }
+
+  user.selectedShip = selectedShipPrevOrigin = shipPointOnMousedown = null; // reset
+});
+
+function rotateButtonHandler(e) {
+  selectedShipElem = e.target.closest(".ship");
+  user.selectedShip = selectedShipElem.id;
+  user.selectedShipOrigin = user.shipOrigin[user.selectedShip];
+
+  removeSelectedShip();
+  rotateSelectedShip();
+
+  user.adjustShipCellOriginToInsideBoard();
+  user.adjustShipCellOriginToAvailableSpace();
+
+  user.addShip();
+
+  user.selectedShip = null; //reset
+}
+
+function removeButtonHandler(e) {
+  selectedShipElem = e.target.closest(".ship");
+  user.selectedShip = selectedShipElem.id;
+  removeSelectedShip();
+
+  user.shipOrientation[user.selectedShip] = "h";
+
+  const menuShipElem = document.querySelector(`.ship-menu__item#${user.selectedShip}`);
+  menuShipElem.classList.remove("ship-menu__item--placed");
+
+  user.selectedShip = null; //reset
+}
+
+const randomizeButton = document.querySelector(".ship-menu__button--random");
+randomizeButton.addEventListener("click", () => {
+  // remove all placed ships
+  for (let ship of shipNames) {
+    if (!user.shipOrigin[ship]) continue; // if not placed
+    user.selectedShip = ship;
+    selectedShipElem = document.querySelector(`.ship#${user.selectedShip}`);
+    removeSelectedShip();
+  }
+
+  user.randomizeShips();
+
+  hideAllShipPopups();
+
+  user.selectedShip = null; // reset
+});
