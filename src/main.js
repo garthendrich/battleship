f96922cf691c7b41b85d5fd3de94439d8b62ade1
Garth Lapitan <garthlapitan.up@gmail.com>
2020-12-10 15:48:12 +0800

    refactor: rename js files

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
+/*
+ * Must include:
+ * Saving and loading the current state of the game using files
+ * File implementation for high scores
+ * A smart AI opponent
+ */
+
+/*
+ TODO
+ * remover of all ships
+ * class User (subclass)
+ * class AI (subclass)
+ * html two boards
+ */
+
 const user = new User();
