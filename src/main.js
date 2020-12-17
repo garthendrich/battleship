a94102a696844897cfe09b36fea43d7168bcd1f6
Garth Lapitan <garthlapitan.up@gmail.com>
2020-12-17 23:10:40 +0800

    Hide ship menu and start game button on start game

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 /*
  TODO
+ * home page
  * ai algorithm
+ * game proper
  */
 
 "use strict";
@@ -11,8 +13,10 @@ const startGameButton = document.querySelector(".start-game");
 startGameButton.addEventListener("click", () => {
   const shipOriginValues = Object.values(user.shipOrigin);
   const areAllShipsPlaced = !shipOriginValues.includes(null);
+  console.log(this);
 
   if (areAllShipsPlaced) {
-    console.log("start game");
+    document.querySelector(".ship-menu").classList.add("ship-menu--hidden");
+    startGameButton.classList.add("start-game--hidden");
   }
 });
