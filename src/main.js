86724e7e7fa60cb3d9098df29157e1c0456e585c
Garth Lapitan <garthlapitan.up@gmail.com>
2020-12-17 22:56:00 +0800

    Add initial startGameButton listener

--- src/main.js
+++ src/main.js
@@ -6,3 +6,13 @@
 "use strict";
 
 const user = new User();
+
+const startGameButton = document.querySelector(".start-game");
+startGameButton.addEventListener("click", () => {
+  const shipOriginValues = Object.values(user.shipOrigin);
+  const areAllShipsPlaced = !shipOriginValues.includes(null);
+
+  if (areAllShipsPlaced) {
+    console.log("start game");
+  }
+});
