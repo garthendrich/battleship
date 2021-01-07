820d2e487da5e290b5f7120d1d55f8a492f65abd
Garth Hendrich Lapitan <garthlapitan.up@gmail.com>
2021-01-07 12:40:04 +0800

    Refactor getNumSailingShips()

--- src/classes/User.js
+++ src/classes/User.js
@@ -6,8 +6,4 @@ class User extends UserSetup {
   canShootEnemyCell([row, column]) {
     return this._shotsTable[row][column] === 0;
   }
-
-  getSailingShips() {
-    return this._shipNames.filter((shipName) => !this.isShipSunk(shipName));
-  }
 }
